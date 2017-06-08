7be65a953986315e2fab6205a7959cd086b8ac55
jfroeschke <john.froeschke@gmail.com>
2017-06-08 10:30:20 -0400

    Commit global file (first commit)

--- global.R
+++ global.R
@@ -1,1 +1,15 @@
 # global.R
+## Developed to display Gulf of Mexico spatial management regulations
+## Data source: http://sero.nmfs.noaa.gov/maps_gis_data/fisheries/gom/GOM_index.html
+
+
+library(shiny)
+library(shinydashboard)
+library(shinyBS)
+library(leaflet)
+library(RColorBrewer)
+library(mapview)
+
+
+### MapBox Attribution##
+map_attr <- "<a href='https://www.mapbox.com/map-feedback/'>Mapbox</a>"
